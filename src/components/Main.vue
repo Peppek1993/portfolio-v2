dd665a7c5315d440b320c111a992a993e1a1c571
Peppek1993 <peppek1993@gmail.com>
2021-05-02 16:12:34 +0200

    Initial setup

---
+++ src/components/Main.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="main-container">
+		<img src="../assets/dark_background.jpeg" alt="Dark Background"/>
+	</div>
+</template>
+
+<script lang="ts" src="./Main.ts"></script>
+
+<style lang="scss" src="./Main.scss" />
